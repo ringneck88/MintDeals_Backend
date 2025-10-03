6ba49bb273b95a5994b30efa4a8b2d2919dc4ffe
Kelly Sharp <kelly.sharp88@gmail.com>
2025-10-03 16:04:42 -0700

    update

---
+++ run-transfer.ps1
@@ -0,0 +1,2 @@
+$env:STRAPI_ADMIN_TRANSFER_TOKEN = "7b9f122f7da6f4c41d9b898088c81caf72446eaf75895b98784beb6d4618a4313fd7b901733c87e4b6d159b2801dc28b866aca56697975ab62a2cc2710927d3b44df3184b10cb810377063c7d82c15d58ad1cc0cb24cba65a30f4acf5f4136efbdd792c0815c403222ef9d1e3562a4f235dcd4ad8dee1af80d510f5ca81603c4"
+npx strapi transfer --from https://mintdealsbackend-production.up.railway.app --from-token 2f468a84-4e37-40c4-bab0-38d1a3676178 --force
